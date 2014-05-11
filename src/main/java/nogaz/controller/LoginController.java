8c0318ccca2b0ae47a22a6b5368bd459c19abc81
BlackJet <brox2319@gmail.com>
2014-05-11 22:24:09 +0400

    ~

---
+++ src/main/java/nogaz/controller/LoginController.java
@@ -0,0 +1,11 @@
+package nogaz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/loginza")
+public class LoginController {
+
+
+}
